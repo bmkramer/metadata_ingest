0f8df4ec15d8346ba9247511bad6eba537c44616
bmkramer <bmkramer@noreply.codeberg.org>
2026-05-04 23:06:45 +0200

    Update databases/temp/202604_open_funding_information_openaire.sql

--- databases/temp/202604_open_funding_information_openaire.sql
+++ databases/temp/202604_open_funding_information_openaire.sql
@@ -1,136 +1,143 @@
 WITH TABLE_SUBSET AS (
 SELECT DISTINCT
 acronym,
-openaire_shortname
+openalex_funder_id as funder_id
 FROM `sos-project-space.FUNDERS_202604.funders_subset_openalex_openaire`
 ),
 
-TABLE_PROJECT_ID AS
-
-(SELECT DISTINCT
-
-b.id as project_id,
-b.funding.shortName,
-EXTRACT(year FROM startDate) as year_start
+TABLE_AWARDS AS (
+SELECT
+a.acronym,
+CONCAT(a.acronym,"|",provenance) as acronym_provenance_uuid,
+b.funder.id as funder_id,
+b.id as award_id,
+b.start_year as award_start_year,
+b.provenance
 
 FROM TABLE_SUBSET as a
-LEFT JOIN (SELECT id, startDate, funding FROM `sos-datasources.openaire.project_20260129`, UNNEST(fundings) as funding) as b
-ON a.openaire_shortname = b.funding.shortName
+LEFT JOIN `subugoe-collaborative.openalex_walden.awards` as b
+ON a.funder_id = b.funder.id
 
-ORDER BY shortName
-
+WHERE b.funder.id is not null
 ),
 
-
-TABLE_JOIN_RELATION AS (
-
+TABLE_AWARDS_AGG AS (
 SELECT
-
-a.* ,
-b.source as product_id
-
-FROM TABLE_PROJECT_ID as a
-LEFT JOIN `sos-datasources.openaire.relation_product_project_20260129` as b
-ON a.project_id = b.target
-
-),
-
-TABLE_PRODUCT AS (
-
-SELECT
-
-id,
-if(pid.scheme = "doi", pid.value, null) as doi,
-EXTRACT (year FROM publicationDate) as publication_year,
-type
-
-FROM `sos-datasources.openaire.publication_20260129`
-LEFT JOIN UNNEST(pids) as pid
-
-UNION ALL
-
-SELECT
-
-id,
-if(pid.scheme = "doi", pid.value, null) as doi,
-EXTRACT (year FROM publicationDate) as publication_year,
-type
-
-FROM `sos-datasources.openaire.dataset_20260129`
-LEFT JOIN UNNEST(pids) as pid
-
-UNION ALL
-
-SELECT
-
-id,
-if(pid.scheme = "doi", pid.value, null) as doi,
-EXTRACT (year FROM publicationDate) as publication_year,
-type
-
-FROM `sos-datasources.openaire.software_20260129`
-LEFT JOIN UNNEST(pids) as pid
-
-UNION ALL
-
-SELECT
-
-id,
-if(pid.scheme = "doi", pid.value, null) as doi,
-EXTRACT (year FROM publicationDate) as publication_year,
-type
-
-FROM `sos-datasources.openaire.otherresearchproduct_20260129`
-LEFT JOIN UNNEST(pids) as pid
-
-),
-
-
-TABLE_JOIN_PRODUCT AS (
-SELECT
-a.*,
-b.* EXCEPT (id)
-
-FROM TABLE_JOIN_RELATION as a
-LEFT JOIN TABLE_PRODUCT as b
-ON a.product_id = b.id
-
-),
-
-TABLE_JOIN_CROSSREF AS (
-
-SELECT
-
-a.*,
-if(b.DOI is not null, true, false) as cr_included,
-b.type as cr_type,
-EXTRACT(YEAR from issued) as cr_year
-
-
-FROM TABLE_JOIN_PRODUCT as a
-LEFT JOIN `sos-datasources.backup.cr_instant_snapshot_20260131` as b
-ON UPPER(TRIM(a.doi)) = UPPER(TRIM(b.DOI))
-
-),
-
-TABLE_AGG AS (
-  
-SELECT
-
-shortName,
-count(distinct project_id) as project_ids,
-MIN(year_start) as project_start_year_min,
-MAX(year_start) as project_start_year_max,
-count(distinct product_id) as product_ids,
----count(distinct if(doi is not null, product_id, null)) as product_ids_with_doi,
----count(distinct doi) as dois,
----count(distinct if(cr_included, doi, null)) as crossref_dois,
-count(distinct if(cr_type = "journal-article", doi, null)) as dois_crossref_journal_article,
-
-FROM TABLE_JOIN_CROSSREF
-
+acronym,
+acronym_provenance_uuid,
+provenance,
+MIN(award_start_year) as award_start_year_min,
+MAX(award_start_year) as award_start_year_max,
+count(award_id) as award_count
+FROM TABLE_AWARDS
 GROUP BY ALL
-ORDER BY shortname
+ORDER BY award_count DESC
+),
+
+TABLE_AWARDS_WORKS AS (
+SELECT DISTINCT
+a.acronym,
+a.acronym_provenance_uuid,
+b.id as work_id,
+b.doi,
+CASE WHEN (SELECT COUNT(1) FROM UNNEST(indexed_in) AS i WHERE i = "crossref") > 0 THEN TRUE ELSE FALSE END as is_crossref_doi,
+IF(b.type IN ("article", "review") AND (source_type = "journal"), TRUE, FALSE) as is_journal_article,
+FROM TABLE_AWARDS as a
+LEFT JOIN (SELECT w.id, w.doi, indexed_in, award, funders, type, primary_location.source.type as source_type FROM `subugoe-collaborative.openalex_walden.works` as w,
+UNNEST (awards) as award) as b
+ON a.award_id = b.award.id
+
+WHERE a.award_id is not null
+),
+
+TABLE_AWARDS_WORKS_AGG_PROVENANCE AS (
+SELECT
+acronym_provenance_uuid, --- to split by provenance
+count(distinct if(work_id is not null, work_id, null)) as work_ids_award,
+---count(distinct if(doi is not null, doi, null)) as dois_award,
+---count(distinct if(doi is not null and is_crossref_doi, doi, null)) as dois_cr_award,
+count(distinct if(doi is not null and is_crossref_doi and is_journal_article, doi, null)) as dois_cr_article_award,
+FROM TABLE_AWARDS_WORKS
+GROUP BY ALL
+),
+
+TABLE_AWARDS_WORKS_AGG_ALL AS (
+SELECT
+acronym, --- to split by acronym
+count(distinct if(work_id is not null, work_id, null)) as work_ids_award,
+---count(distinct if(doi is not null, doi, null)) as dois_award,
+---count(distinct if(doi is not null and is_crossref_doi, doi, null)) as dois_cr_award,
+count(distinct if(doi is not null and is_crossref_doi and is_journal_article, doi, null)) as dois_cr_article_award,
+FROM TABLE_AWARDS_WORKS
+GROUP BY ALL
+),
+
+TABLE_JOIN_AWARDS_PROVENANCE AS (
+SELECT
+a.* EXCEPT (acronym_provenance_uuid),
+b.* EXCEPT (acronym_provenance_uuid)
+FROM TABLE_AWARDS_AGG as a
+LEFT JOIN TABLE_AWARDS_WORKS_AGG_PROVENANCE as b
+USING (acronym_provenance_uuid)
+ORDER BY acronym
+),
+
+TABLE_FUNDERS_WORKS AS (
+SELECT DISTINCT
+a.acronym,
+b.id as work_id,
+b.doi,
+CASE WHEN (SELECT COUNT(1) FROM UNNEST(indexed_in) AS i WHERE i = "crossref") > 0 THEN TRUE ELSE FALSE END as is_crossref_doi,
+IF(b.type IN ("article", "review") AND (source_type = "journal"), TRUE, FALSE) as is_journal_article,
+FROM TABLE_SUBSET as a
+LEFT JOIN (SELECT w.id, w.doi, indexed_in, funder, type, primary_location.source.type as source_type FROM `subugoe-collaborative.openalex_walden.works` as w,
+UNNEST (funders) as funder) as b
+ON a.funder_id = b.funder.id
+WHERE a.funder_id is not null
+),
+
+TABLE_FUNDERS_WORKS_AGG AS (
+SELECT
+acronym,
+count(distinct if(work_id is not null, work_id, null)) as work_ids_funder,
+--count(distinct if(doi is not null, doi, null)) as dois_funder,
+--count(distinct if(doi is not null and is_crossref_doi, doi, null)) as dois_cr_funder,
+count(distinct if(doi is not null and is_crossref_doi and is_journal_article, doi, null)) as dois_cr_article_funder,
+FROM TABLE_FUNDERS_WORKS
+GROUP BY ALL
+),
+
+TABLE_UNION AS (
+---awards and works linked to awards, by provenance
+SELECT
+acronym,
+provenance,
+award_start_year_min,
+award_start_year_max,
+award_count,
+work_ids_award,
+dois_cr_article_award,
+null as work_ids_funder_only,
+null as dois_cr_article_funder_only
+FROM TABLE_JOIN_AWARDS_PROVENANCE
+
+UNION ALL
+
+--- works linked to awards, all
+--- include works linked to funders only
+SELECT
+a.acronym,
+"all" as provenance,
+null as award_start_year_min,
+null as award_start_year_max,
+null as award_count,
+a.work_ids_award,
+a.dois_cr_article_award,
+(b.work_ids_funder- a.work_ids_award) as work_ids_funder_only,
+(b.dois_cr_article_funder - a.dois_cr_article_award) as dois_cr_article_funder_only
+FROM TABLE_AWARDS_WORKS_AGG_ALL as a
+LEFT JOIN TABLE_FUNDERS_WORKS_AGG as b 
+USING (acronym)
 )
 
-SELECT * FROM TABLE_AGG
+SELECT * FROM TABLE_FUNDERS_WORKS_AGG

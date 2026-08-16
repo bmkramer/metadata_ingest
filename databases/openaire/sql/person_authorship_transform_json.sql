37a914db330ebbf1159b823fd3dc83342923d418
bmkramer <bmkramer@noreply.codeberg.org>
2026-08-16 15:12:20 +0200

    Add script for post-processing array imported as json

---
+++ databases/openaire/sql/person_authorship_transform_json.sql
@@ -0,0 +1,8 @@
+SELECT 
+
+* EXCEPT (roles),
+JSON_VALUE_ARRAY(JSON_STRIP_NULLS(roles)) as roles_strip
+--- JSON_STRIP_NULLS removes null values within arrays
+--- cases where variable is null will be transformed into empty arrays
+
+FROM `sos-datasources.upload.openaire_person_authorship_202608` 
